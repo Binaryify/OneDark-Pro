ee8f6b7cfa4da2c0398f0cd26115c7454c8c7883
Scott M. Wyant <29161635+scottmwyant@users.noreply.github.com>
2020-06-14 11:34:20 -0400

    Define an interface TokenColor
    Use an interface to define the shape of the data used to style
    code in the editor.

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,2 +1,3 @@
 export * from './colors'
 export * from './configuration'
+export * from './TokenColor'
--- src/themes/syntax.ts
+++ src/themes/syntax.ts
@@ -1,21 +1,22 @@
 import Colors from '../interface/colors'
+import { TokenColor } from '../interface'
 import * as data from './themeData.json'
 // import { cloneDeep } from '../utils/cloneDeep'
 import { uniqBy } from '../utils/uniqBy'
 
 export default function (configuration) {
 
-  let result: any = data.tokenColors.default
+  let result: TokenColor[] = data.tokenColors.default
 
   if (configuration.bold) {
     result = uniqBy([...data.tokenColors.bold, ...result], setting => {
       return setting.name + setting.scope
-    })
+    }) as TokenColor[]
   }
   if (configuration.italic) {
     result = uniqBy([...data.tokenColors.italic, ...result], setting => {
       return setting.name + setting.scope
-    })
+    }) as TokenColor[]
   }
 
   // Fill in color placeholders with concrete color values
